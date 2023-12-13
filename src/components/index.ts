b068adbc6344e03cc9b8601c6b226c91e84d99c0
wangli <wangli@zhiannet.com>
2023-12-13 15:43:03 +0800

    feat: 全局组件封装

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,13 +2,13 @@
 import SvgIcon from '@/components/Svgicon.vue'
 const allGlobalComponent = {
   SvgIcon,
-};
+}
 export default {
   install(app) {
-    console.log("install");
-    
-    console.log(app);
-    
+    console.log('install')
+
+    console.log(app)
+
     Object.keys(allGlobalComponent).forEach((key) => {
       app.component(key, allGlobalComponent[key])
     })
